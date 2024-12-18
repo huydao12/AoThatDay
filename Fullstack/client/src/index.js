afa01e734100770f0d161c7720f8896b9fa37bca
quockhanhzzzz <tranquockhanh02099@gmail.com>
2024-12-18 17:51:27 +0700

    Update index.js

--- Fullstack/client/src/index.js
+++ Fullstack/client/src/index.js
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store, { persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 import "./index.scss";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const queryClient = new QueryClient();
+
 root.render(
   // <React.StrictMode>
   <QueryClientProvider client={queryClient}>
@@ -19,9 +20,7 @@ root.render(
         <App />
       </PersistGate>
     </Provider>
-    <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>
-
   // </React.StrictMode>
 );
 
